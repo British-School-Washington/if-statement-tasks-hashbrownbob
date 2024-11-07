cf10668401e89cbe7e6e02dc62c1f1ae352c4504
hashbrownbob <alexander.hake@outlook.com>
2024-11-07 12:46:15 -0500

    Update starter-task-3.py

--- starter-task-3.py
+++ starter-task-3.py
@@ -1,10 +1,8 @@
-# Ask the user for their age
+print("your age?")
 age = int(input("Enter your age: "))
 
-# if the user is eligible to vote (age 18 or older)
 if age >= 18:
-    # TODO: Print a message saying they are eligible to vote
+    print("you can vote")
 
-# if the user is not eligible to vote (under 18)
 else:
-    # TODO: Print a message saying they are not eligible to vote
+    print("no vote for you")
